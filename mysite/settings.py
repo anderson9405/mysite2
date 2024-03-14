e478048033fcfa6080ff51105991051e36b6835d
Anderson <anderson052594@gmail.com>
2024-03-13 21:26:58 -0500

    octavo commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
